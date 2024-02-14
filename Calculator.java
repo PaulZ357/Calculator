ad26e49532dc785b417b881b37cd233cb3eefe71
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-02-13 23:12:21 -0500

    added support for floating point numbers

--- Calculator.java
+++ Calculator.java
@@ -13,16 +13,26 @@ public class Calculator {
         System.out.print("Do you want to add, subtract, multiply, or divide? ");
         String input3 = keyboard.nextLine();
         double answer = 0;
-        if (input3.equals("add") || input3.equals("a")) {
-            answer = input1 + input2;
-        } else if (input3.equals("subtract") || input3.equals("s")) {
-            answer = input1 - input2;
-        } else if (input3.equals("multiply") || input3.equals("m")) {
-            answer = input1 * input2;
-        } else if (input3.equals("divide") || input3.equals("d")) {
-            answer = input1 / input2;
+        boolean divide = false;
+        try {
+            if (input3.equals("add") || input3.equals("a")) {
+                answer = input1 + input2;
+            } else if (input3.equals("subtract") || input3.equals("s")) {
+                answer = input1 - input2;
+            } else if (input3.equals("multiply") || input3.equals("m")) {
+                answer = input1 * input2;
+            } else if (input3.equals("divide") || input3.equals("d")) {
+                divide = true;
+                answer = input1 / input2;
+            }
+            System.out.println("answer: " + answer);
+        } catch (ArithmeticException e) {
+            if (divide) {
+                System.out.println("Cannot divide by 0");
+            } else {
+                e.printStackTrace();
+            }
         }
-        System.out.print("answer: " + answer);
         keyboard.close();
     }
 }
