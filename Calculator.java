12e4e35626ba9dc11cd2461fcb40bab9d8555720
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-02-13 22:36:37 -0500

    added addition

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter first input: ");
+        int input1 = keyboard.nextInt();
+        System.out.print("Enter second input: ");
+        int input2 = keyboard.nextInt();
+        int answer = input1 + input2;
+        System.out.print("answer: " + answer);
+        keyboard.close();
     }
 }
