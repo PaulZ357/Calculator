346f9ef17a9b6cf62ab6cdd2b17c500725bd1169
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-02-13 22:50:06 -0500

    added subtraction & prompt

--- Calculator.java
+++ Calculator.java
@@ -7,7 +7,17 @@ public class Calculator {
         int input1 = keyboard.nextInt();
         System.out.print("Enter second input: ");
         int input2 = keyboard.nextInt();
-        int answer = input1 + input2;
+
+        keyboard.nextLine(); // i think this is unnecessary, but this is the only way I can get nextLine to work
+
+        System.out.print("Do you want to add or subtract?: ");
+        String input3 = keyboard.nextLine();
+        int answer = 0;
+        if (input3.equals("add") || input3.equals("a")) {
+            answer = input1 + input2;
+        } else if (input3.equals("subtract") || input3.equals("s")) {
+            answer = input1 - input2;
+        }
         System.out.print("answer: " + answer);
         keyboard.close();
     }
